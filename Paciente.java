783c1e8dd963ae6a7592a497a5bfb951ff58db76
Estheban Heredia <esthebanheredia15@gmail.com>
2024-09-15 19:44:42 -0600

    Se añadió el constructor de la clase paciente

--- Paciente.java
+++ Paciente.java
@@ -6,4 +6,12 @@ public class Paciente implements Serializable {
     private String nombre;
     private String doctor;
     private String clinica;
+
+    public Paciente(String id, String nombre, String doctor, String clinica) {
+        this.id = id;
+        this.nombre = nombre;
+        this.doctor = doctor;
+        this.clinica = clinica;
+    }
+
 }
