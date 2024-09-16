d8828f2676196a1fa39ee0b4c1ec70abafb13db7
Estheban Heredia <esthebanheredia15@gmail.com>
2024-09-15 19:40:09 -0600

    Se añadió la base para la clase Paciente con sus respectivas variables

---
+++ Paciente.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class Paciente implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String nombre;
+    private String doctor;
+    private String clinica;
+}
