ef338fba868392432ead4bf213256239f6bf0a66
VictorGomez21003 <boc21003@uvg.edu.gt>
2024-09-16 23:39:09 -0600

    volver publico el nombre

--- Clinica.java
+++ Clinica.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 
 public class Clinica implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; 
     private String nombre;
     private String direccion;
 
