a6f6a2c5e6f1ddfc573ad4f81baeb4eb92a42216
VictorGomez21003 <boc21003@uvg.edu.gt>
2024-09-17 17:06:04 -0600

    Update MainProyecto.java

--- MainProyecto.java
+++ MainProyecto.java
@@ -1,1 +1,9 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    // Nombre del archivo CSV para almacenar todos los datos
+    private static final String FILE_NAME = "Proyecto.csv";
+    private static final String DELIMITER = ",";  // Delimitador para separar campos en el CSV
+  
