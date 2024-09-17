2c0bc2ee71fb1d9aeb4e91a3f50cb918671ade6a
unknown <gon241214@uvg.edu.gt>
2024-09-17 10:40:54 -0600

    Getter para el nombre del doctor.

--- Paciente.java
+++ Paciente.java
@@ -7,7 +7,7 @@ public class Paciente implements Serializable {
     private String doctor;
     private String clinica;
 
-    //Constructor de la clase Paciente
+    // Constructor de la clase Paciente
     public Paciente(String id, String nombre, String doctor, String clinica) {
         this.id = id;
         this.nombre = nombre;
@@ -15,25 +15,30 @@ public class Paciente implements Serializable {
         this.clinica = clinica;
     }
 
-    //Getters y Setters de la clase
+    // Getters y Setters de la clase
 
-    //Devuelve el valor del ID del paciente.
+    // Devuelve el valor del ID del paciente.
     public String getId() {
         return id;
     }
 
-    //Setter que permite modificar el valor del ID del paciente.
+    // Setter que permite modificar el valor del ID del paciente.
     public void setId(String id) {
         this.id = id;
     }
 
-    //Getter que devuelve el nombre del paciente.
+    // Getter que devuelve el nombre del paciente.
     public String getNombre() {
         return nombre;
     }
 
-    //Setter que permite modificar el valor del nombre del paciente.
+    // Setter que permite modificar el valor del nombre del paciente.
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    // Este metodo funciona para obtener el nombre del doctor.
+    public String getDoctor() {
+        return doctor;
     }
 }
