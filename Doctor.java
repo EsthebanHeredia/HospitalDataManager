adc6d58a0093e01d1153a055d53682d3d2dc7fed
silversitoooo <164232812+silversitoooo@users.noreply.github.com>
2024-09-16 19:47:19 -0600

    segundo commit

--- Doctor.java
+++ Doctor.java
@@ -1,1 +1,7 @@
 import java.io.Serializable;
+
+public class Doctor implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String nombre;
+    private String clinica;
