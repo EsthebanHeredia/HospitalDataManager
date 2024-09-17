6b9ec5555d446381a9278a12e51618e572d8935d
VictorGomez21003 <boc21003@uvg.edu.gt>
2024-09-16 23:37:25 -0600

    agregar nombre de la clinica y dirección

--- Clinica.java
+++ Clinica.java
@@ -1,1 +1,36 @@
+import java.io.Serializable;
+
+public class Clinica implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String nombre;
+    private String direccion;
+
+    public Clinica(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    // Métodos getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + "," + direccion;
+    }
+}
+
 
