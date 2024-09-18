22ff6476067a5d2d07d0139e3e0839dc8a77a7a3
adrianArimany <ari211063@uvg.edu.gt>
2024-09-17 18:01:46 -0600

    Update Paciente.java
    Cambie los comentarios de // a /**/ para que se complien adecuadamente.

--- Paciente.java
+++ Paciente.java
@@ -7,7 +7,15 @@ public class Paciente implements Serializable {
     private String doctor;
     private String clinica;
 
-    // Constructor de la clase Paciente
+    /**
+     * Constructor de la clase Paciente.
+     * Inicializa un nuevo objeto Paciente con la información proporcionada.
+     *
+     * @param id     El identificador único del paciente
+     * @param nombre El nombre del paciente
+     * @param doctor El nombre del doctor asignado al paciente
+     * @param clinica El nombre de la clínica donde se atiende al paciente
+     */
     public Paciente(String id, String nombre, String doctor, String clinica) {
         this.id = id;
         this.nombre = nombre;
@@ -15,29 +23,47 @@ public class Paciente implements Serializable {
         this.clinica = clinica;
     }
 
-    // Getters y Setters de la clase
-
-    // Devuelve el valor del ID del paciente.
+    /**
+     * Obtiene el ID del paciente.
+     *
+     * @return El ID del paciente.
+     */
     public String getId() {
         return id;
     }
 
-    // Setter que permite modificar el valor del ID del paciente.
+    /**
+     * Establece el ID del paciente.
+     *
+     * @param id El nuevo ID a asignar al paciente.
+     */
     public void setId(String id) {
         this.id = id;
     }
 
-    // Getter que devuelve el nombre del paciente.
+    /**
+     * Obtiene el nombre del paciente.
+     *
+     * @return El nombre del paciente.
+     */
     public String getNombre() {
         return nombre;
     }
 
-    // Setter que permite modificar el valor del nombre del paciente.
+    /**
+     * Establece el nombre del paciente.
+     *
+     * @param nombre El nuevo nombre a asignar al paciente.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    // Este metodo funciona para obtener el nombre del doctor.
+    /**
+     * Obtiene el nombre del doctor asignado al paciente.
+     *
+     * @return El nombre del doctor.
+     */
     public String getDoctor() {
         return doctor;
     }
