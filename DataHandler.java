86b1cee34d4ba36cb3f5525c2e8affeae7eeaf21
Estheban Heredia <esthebanheredia15@gmail.com>
2024-10-13 18:54:25 -0600

    Constructor del DataHandler
    .

---
+++ DataHandler.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataHandler {
+
+    private static final String DELIMITER = ",";
+    private static final String CLINICA_FILE = "clinicas.csv";
+    private static final String DOCTOR_FILE = "doctores.csv";
+    private static final String PACIENTE_FILE = "pacientes.csv";
+
+}
