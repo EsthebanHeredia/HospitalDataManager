ac3cb32bd800c045424c738e93747c698d4701eb
adrianArimany <ari211063@uvg.edu.gt>
2024-10-14 19:19:11 -0600

    Javadoc para Authenticator.java
    Describe el metodo verifyCredenctials().

--- Authenticator.java
+++ Authenticator.java
@@ -2,6 +2,12 @@ public class Authenticator {
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "password";
 
+    /**
+     * Verifica la contrasena y usuario del administrador.
+     * @param username del administrador
+     * @param password del adminstrador
+     * @return true = username y password are correct; false = username and password are inccorect.
+     */
     public static boolean verifyCredentials(String username, String password) {
         return USERNAME.equals(username) && PASSWORD.equals(password);
     }
