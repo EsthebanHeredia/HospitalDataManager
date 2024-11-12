f1ae220ff6f9353e10d81f1ff460b094f19920e7
Estheban Heredia <esthebanheredia15@gmail.com>
2024-11-12 11:48:32 -0600

    getters

--- Administrador.java
+++ Administrador.java
@@ -13,4 +13,27 @@ public class Administrador implements Serializable {
         this.nombre = nombre;
         this.contrasena = contrasena;
     }
+
+    //Getters
+    public String getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getContrasena() { return contrasena; }
+
+    public static void iniciarSesion(Scanner scanner, DataHandler dataHandler) {
+        System.out.print("Ingrese su usuario: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Ingrese su contraseña: ");
+        String contrasena = scanner.nextLine();
+
+        AuthResult<Administrador> authResult = new Authenticator(dataHandler)
+            .<Administrador>autenticar("administrador", nombre, contrasena, "");
+            
+        if (authResult.isAuthenticated()) {
+            Administrador admin = authResult.getUser();
+            admin.menuAdministrador(scanner, dataHandler);
+        } else {
+            System.out.println("Credenciales inválidas");
+            esperarEnter(scanner);
+        }
+    }
 }
