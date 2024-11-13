389eb99d28a2ddd6d03e3c3ea03c0cb7b38ee64c
silversitoooo <164232812+silversitoooo@users.noreply.github.com>
2024-11-12 18:50:41 -0600

    buscarPacienteporId

--- DataHandler.java
+++ DataHandler.java
@@ -56,6 +56,29 @@ public class DataHandler {
             System.err.println("Error en formato de archivo de clínicas: " + e.getMessage());
         }
     }
+        public Paciente buscarPacientePorId(String id) {
+        try (BufferedReader br = new BufferedReader(new FileReader(PACIENTE_CSV))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = parseCSVLine(line);
+                if (data.length > 0 && data[0].equals(id)) {
+                    Paciente paciente = new Paciente(data[0], data[1]);
+                    if (data.length > 2) {
+                        paciente.setHistorialMedico(Paciente.parseHistorialMedico(data[2]));
+                    }
+                    if (data.length > 3) {
+                        paciente.setCitasMedicas(Paciente.parseCitasMedicas(data[3]));
+                    }
+                    return paciente;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error buscando paciente: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Error en formato de archivo de pacientes: " + e.getMessage());
+        }
+        return null;
+    }
 }
 
 
