0f688cdf829e8ea8bff53911d92039f956b92e99
VictorGomez21003 <boc21003@uvg.edu.gt>
2024-11-12 18:36:07 -0600

    LimpiarPantalla

--- Doctor.java
+++ Doctor.java
@@ -152,5 +152,10 @@ public class Doctor implements Serializable {
         System.out.println("\nPresione Enter para continuar...");
         scanner.nextLine();
     }
+
+        private void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     
 }
