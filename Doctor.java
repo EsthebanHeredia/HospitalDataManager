61fc59535e052f8c9225fa829eaaef098f0f2e3b
VictorGomez21003 <boc21003@uvg.edu.gt>
2024-11-12 18:37:16 -0600

    corrección de funciones

--- Doctor.java
+++ Doctor.java
@@ -147,15 +147,5 @@ public class Doctor implements Serializable {
         }
     }
     }
-
-        private static void esperarEnter(Scanner scanner) {
-        System.out.println("\nPresione Enter para continuar...");
-        scanner.nextLine();
-    }
-
-        private void limpiarPantalla() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
     
 }
