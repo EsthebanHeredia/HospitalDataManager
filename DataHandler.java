85564656d94f5fba008f5deead0bb7905d9fd949
VictorGomez21003 <boc21003@uvg.edu.gt>
2024-11-12 18:41:46 -0600

    ver Pacientes

--- DataHandler.java
+++ DataHandler.java
@@ -6,6 +6,22 @@ public class DataHandler {
     private static final String DOCTOR_CSV = "doctores.csv";
     private static final String ADMIN_CSV = "administradores.csv";
     private static final String CLINICA_CSV = "clinicas.csv";
+
+        public void verPacientes() {
+        try (BufferedReader br = new BufferedReader(new FileReader(PACIENTE_CSV))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = parseCSVLine(line);
+                if (data.length > 0 && !data[0].equalsIgnoreCase("ID")) {
+                    System.out.println("ID: " + data[0] + ", Nombre: " + data[1]);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error leyendo pacientes: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Error en formato de archivo de pacientes: " + e.getMessage());
+        }
+    }
 }
 
 
